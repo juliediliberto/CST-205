fad8be2f462b35ebc35aba215306a54d05913da9
Julia Diliberto <jdiliberto@csumb.edu>
2014-11-30 17:47:27 -0800

    lab 12.py as of 11/30

---
+++ lab12.py
@@ -0,0 +1,129 @@
+# Prints game instructions
+def printInstructions():
+  printNow(
+  """***Welcome to our home!***
+  Imagine you are the parent of four teenaged boys.  You find yourself in the
+  kitchen one Saturday morning with a desire to make pancakes for the family.
+  This ideas poses several challenges.  First, the milk is not in the kitchen.
+  Second, several dishes are missing from the kitchen and finally, you're not
+  sure how many people spend the night last night and will eat pancakes. 
+  In each room, a description of the room will be displayed.
+  You can choose to move some direction by typing \"north\", \"south\",
+  \"east\" or \"west\".  You may type \"pick up\" if you want to pick up
+  something in a room. You may type \"count\" to add the occupants 
+  of the room to your total count. Type \"help\" if you want to see the 
+  instructions again. Type "exit" if you want to leave the game.  Good luck!""")
+
+# Prints a different message depending on the location
+def locationMessage(location):
+
+# Define location messages
+  KitchenMessage = """You are now in the kitchen.  You may exit through a door to the west or to the south.
+  Come back here to make the pancakes once you have the missing supplies and know how
+  many people are home."""
+  
+  MasterBedroomMessage = """You are now in the master bedroom.  There's nobody in here.  You may exit
+  through the door on the east wall or the door on the south wall."""
+  
+  DiningRoomMessage = """You are now in the dining room.  Someone left the milk on the table, but there's
+  no one here now. There are two doors, one on the north wall and one on the west wall."""
+  
+  LivingRoomMessage = """You are now in the living room.  You see three teenaged boys playing video games
+  and shoes everywhere.  You may exit through the door on the north wall or the door on the west wall."""
+  
+  MattNNicksRoomMessage = """You are now in Matt and Nick's room.  There are several plates on the shelf and
+  Nick is still asleep.  Oh wait, there's Collin too, in a sleeping bag on the floor barely
+  visible amongst the dirty clothes.  There are two doors, one on the east wall and one on the south wall."""
+  
+  RyanNJaredsRoomMessage = """You are now in Ryan and Jared's room.  You see Ryan studying for his
+  Chemistry test. There is one door on the north wall."""
+  
+  if location == 'the kitchen':
+    printNow(KitchenMessage)
+  elif location == 'the masterBedroom':
+    printNow(MasterBedroomMessage)
+  elif location == 'the dining room':
+    printNow(DiningRoomMessage)
+  elif location == 'the living room':
+    printNow(LivingRoomMessage)
+  elif location == 'Matt & Nick\'s room':
+    printNow(MattNNicksRoomMessage)
+  else:
+    printNow(RyanNJaredsRoomMessage)
+    
+# Changes milkPickedUp or dishesPickedUp if appropriate
+def updatePickedUp(location):
+  if location == "the dining room":
+    milkPickedUp == true
+  elif location == "Matt & Nick's room":
+    dishesPickedUp == true
+  else:
+    printNow ("There's nothing you need for pancakes in here.")
+    
+# Updates the count of people in the house
+def updateCount(location):
+  if location == 'the living room':
+    count = count + 3
+  elif location == 'Ryan & Jared\'s room':
+    count = count + 1
+  elif location == 'Matt & Nick\'s room':
+    count = count + 2
+  else:
+    printNow ("There's nobody in here to count")
+  return count
+# Requests, validates and stores user input regarding next move
+# Possiblemoves = ['north','south','east','west','help','quit']
+def getMove():
+
+  return move
+  
+def changeLocation(location, move):
+
+  return location
+# Prints ending message
+def finishGame(finished):
+
+# Define ending messages
+  QuitMessage = "Tough job, huh? Thanks for trying."
+  WinMessage = "You did it! Now you can make pancakes, unless it's time to take Jared to basketball practice."
+  if finished:
+    printNow(QuitMessage)
+  else:
+    printNow(WinMessage)
+
+def playGame():
+# Initialize location, finished, count, dishesPickedUp and milkPickedUp variables
+  location = "the kitchen"
+  finished = false
+  count = 1
+  dishesPickedUp = false
+  milkPickedUp = false
+  
+# Print game instructions
+  printInstructions()
+  
+# Game play 
+  while not finished and not(location == "the kitchen" and count == 7 and dishesPickedUp and milkPickedUp):
+    locationMessage(location)    
+    move = getMove()
+    if move == 'quit':
+      finished = true
+    elif move == 'help':
+      printInstructions()
+    elif move == 'count':
+      count = updateCount(location)
+    elif move == 'pick up'
+      if location == 'dining room':
+        milkPickedUp = true
+      elif location == 'Matt & Nick\'s room':
+        dishedPickedUp = true
+      else:
+        printNow ("There's nothing you need for pancakes in here.")
+    else:
+      location = changeLocation(location,move)
+
+# Print ending message  
+  finishGame(finished)
+    
+
+  

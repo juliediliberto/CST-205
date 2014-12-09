7aaddee3507fcfe13e2f996535dd20d4de3da4a0
Julia Diliberto <jdiliberto@csumb.edu>
2014-12-08 22:41:47 -0800

    removes special characters now

--- lab14b.py
+++ lab14b.py
@@ -1,16 +1,23 @@
 # CST 205
-# Lab #14
+# Lab #14b
 # Contributors:  Robert Contreras, Julia Diliberto, Matthew Bozelka
 # December 6, 2014
 # Stipulations:  
 
 
+def removeSpecialChars(string):
+  html_escape_table = {
+     "&#8230;": "...",
+     "&nbsp;": " "}
+  for key, value in html_escape_table.iteritems():
+    string = string.replace(key, value)
+  return string
+  
 def titleSearch():
   webPagePath = 'C:\\Users\\GRAM\\Documents\\CST 205\\Lab 14\\DOCTYPE html.txt'
   page = open (webPagePath,"rt")
   pageString = page.read()  
   
-
 # Create list to contain found titles
   titles = []
   
@@ -18,22 +25,19 @@ def titleSearch():
 
   begindex = 0
   start = 0
+  begmarker = "rel=\"bookmark\">"
+  endmarker = "</a>"
   finish = len(pageString)
   while begindex != -1:
-    begindex = pageString.find("rel=\"bookmark\">", start, finish)
+    begindex = pageString.find(begmarker, start, finish)
     if begindex != -1:
-      begindex += 16
+      begindex += len(begmarker)
     endindex = pageString.find("</a>",begindex, finish)
     endindex -= 1
     titles.append(pageString[begindex:endindex])
     start = endindex
-    
-# Remove unwanted characters
-  for i in range(0,len(titles)):
- #   index = titles[i].find("&nbsp;")
-    printNow(titles[i])
-    titles[i].replace("&nbsp;", " ")# = (titles[i][:index] + titles[i][index+6:])
-    titles[i].replace("\x92", "\'")
-    printNow(titles[i])
-# Output titles
-# printNow ( titles)
+     
+# Remove unwanted characters and output titles
+  for eachString in titles:
+    eachString = removeSpecialChars(eachString)
+    printNow (eachString)
